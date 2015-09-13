17c32ebb474865c3a33f4b4bdb1b40bbaa6fde10
Xavier Durand-Smet <xavier.durand-smet>
2015-09-12 18:30:13 -0700

    changed template

--- server/app/routes/message.js
+++ server/app/routes/message.js
@@ -8,16 +8,16 @@ var sendMessage = require('../mailServers/sendEmail.js'); //takes: to_name, to_e
 var fs = require('fs')
 
 // get a template buffer, convert it to string, pass it as mail canned response
-fs.readFile('/Users/xavierdurandsmet/CodingChallenges/mailServer/server/app/mailServers/index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data.toString('utf-8'));
-  var mailTemplate = data.toString('utf-8')
+// fs.readFile('/Users/xavierdurandsmet/CodingChallenges/mailServer/server/app/mailServers/index.html', function (err, data) {
+//   if (err) throw err;
+//   console.log(data.toString('utf-8'));
+//   var mailTemplate = data.toString('utf-8')
 
   	  // receive call from front-end
 	  router.post('/sendMail', function (req,res,next) {
 	  console.log('req.body', req.body)
-	  // var mailTemplate = '<h1>Thanks so much for applying ' + req.body.firstName +'</h1><h2>See you soon on our website!</h2>'
+	  var mailTemplate = '<h1>Thanks so much for applying ' + req.body.firstName +'</h1><h2>See you soon on our website!</h2>'
 	  sendMessage(req.body.firstName, req.body.email, 'myNameTest', 'myEmailTest@test.com', 'Thanks for joining us!', mailTemplate)
 	  res.json(req.body) 
 	})
-});
+// });
